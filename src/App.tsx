8dad288e7345882fd0cf190f074ba50c63baa23e
Alvin <alvinyongho@gmail.com>
2021-07-11 17:40:31 -0700

    Webgl canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,90 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type Wasm = typeof import("wasm");
+
+interface CanvasProps {
+  wasm: Wasm;
+}
+
+
+function startRenderer(gl: WebGLRenderingContext, canvas: HTMLCanvasElement, wasm: Wasm) {
+  // Support semi-transparent objects (Performance cost)
+  gl.enable(gl.BLEND);
+  // Blend function (how transparent it is)
+  gl.blendFunc(gl.SRC_ALPHA,gl.ONE_MINUS_SRC_ALPHA);
+
+  const FPS_THROTTLE = 1000.0 / 30.0; // milliseconds / frames
+  let lastDrawTime = -1;
+  const initialTime = Date.now();
+
+  const game = new wasm.Game(1, 1);
+
+  // When to render.
+  function render() {
+    // Render on the next animation frame.
+    window.requestAnimationFrame(render);
+    const currTime = Date.now();
+
+    if (currTime >=lastDrawTime+FPS_THROTTLE) {
+      lastDrawTime = currTime;
+      if (window.innerHeight != canvas.height || window.innerWidth != canvas.width) {
+        canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth;
+        gl.viewport(0,0,window.innerWidth, window.innerHeight);
+      }
+      
+      let elapsedTime = currTime-initialTime;
+      game.update(elapsedTime, window.innerHeight, window.innerWidth);
+      game.render();
+      // Rust Update call. ie. collisions
+      // Rust Render call. creating an image
+    }
+  }
+  render();
+}
+
+const Canvas = (props: CanvasProps) => {
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+  const [context, setContext] = React.useState<WebGLRenderingContext | null>(
+    null
+  );
+
+  React.useEffect(() => {
+    if (canvasRef.current) {
+      const gl = canvasRef.current.getContext('webgl', {antialias: true});
+
+      if (gl) {
+        startRenderer(gl, canvasRef.current, props.wasm);
+        setContext(gl);
+      }
+    }
+  }, [context]);
+
+  return (
+    <canvas ref={canvasRef} {...props}></canvas>
+  );
+}
+
+
 function App() {
+  const [wasmImport, setWasmImport] = React.useState<typeof import("wasm") | null>(null);
+
   useEffect( () => {
     (async()=>{
       const module  = await import("wasm");
-      const result = module.add(1,2);
-      console.log(result);
+      setWasmImport(module);
     })();
   }, []);
+
+  if(!wasmImport) {
+    return (<div>Loading...</div>);
+  } 
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas wasm={wasmImport}></Canvas>
     </div>
   );
 }
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,44 @@
 // The wasm-pack uses wasm-bindgen to build and generate JavaScript binding file.
 // Import the wasm-bindgen crate.
+use std::f64;
+
 use wasm_bindgen::prelude::*;
+use blackjack::card::card::*;
+use blackjack::player::player::*;
+use wasm_bindgen::JsCast;
+
+// lifted from the `console_log` example
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+#[derive(Copy, Clone)]
+pub enum Suit {
+  HEART, 
+  DIAMOND, 
+  SPADE,
+  CLUB,
+}
+
+#[wasm_bindgen]
+#[derive(Clone)]
+pub struct Card {
+  suit: Suit,
+  value: String,
+  position_x: f64,
+  position_y: f64,
+}
+
+#[wasm_bindgen]
+pub struct Game {
+  players: usize,
+  deck: usize,
+  // cards: Vec<Card>,
+}
+
 
 // Our Add function
 // wasm-pack requires "exported" functions
@@ -8,4 +46,41 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
   return a + b;
+}
+
+#[wasm_bindgen]
+pub fn create_game(players: usize, deck: usize) {
+  // A single deck of cards
+  let mut _deck = Deck::new(1);
+
+  let mut players: Vec<Player> = Vec::with_capacity(players);
+  let player_a = Player::new(String::from("Player A"), false, 1000);
+  let player_b = Player::new(String::from("Player B"), false, 1000);
+  players.push(player_a);
+  players.push(player_b);
+
+  let _dealer = Player::new(String::from("Dealer"), true, -1);
+}
+
+#[wasm_bindgen]
+impl Game {
+  #[wasm_bindgen(constructor)]
+  pub fn new(players: usize, deck: usize) -> Self {
+
+    //TODO generate card and place them in the game.
+    Self {
+      players: players,
+      deck: deck,
+    }
+  }
+
+  pub fn update(&mut self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {  
+    Ok(())
+  }
+
+  pub fn render(&self) {
+    log("Render was hit");
+
+  }
+
 }
